2f2e54bb7e6f2cde9379097601b0a47e275e87f6
Casey Zimmer <czimmer@ramapo.edu>
2018-08-15 20:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,7 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
+    
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
